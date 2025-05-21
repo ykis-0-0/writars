534e8a01963539530873baff5f02cdc606324096
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2025-05-21 15:14:58 +0000

    Add not-so-sanity check

--- src/cli/checks.rs
+++ src/cli/checks.rs
@@ -3,12 +3,22 @@ use eyre::Result as eResult;
 use crate::lib;
 
 const THE_SPECIMEN: &'static str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/mocks/extensive.ron"));
+const THE_SANITY_CHECK: &'static str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/mocks/minimal.ron"));
 
 #[test]
 fn test_sample_deserialize() -> eResult<()> {
   let parse_result = super::get_ron_spec().from_str::<lib::TarfileSpec>(THE_SPECIMEN)?;
 
-  println!("{:?}", parse_result);
+  println!("{:#?}", parse_result);
+
+  Ok(())
+}
+
+#[test]
+fn test_sanity() -> eResult<()> {
+  let parse_result = super::get_ron_spec().from_str::<lib::TarfileSpec>(THE_SANITY_CHECK)?;
+
+  println!("{:#?}", parse_result);
 
   Ok(())
 }
